8d563aa5b0148eef1329246dcf84a841b9bc80f6
Makoto Tanabe <xekid78@outlook.com>
2018-01-18 23:54:09 +0900

    変更・削除を追加

--- sample14.java
+++ sample14.java
@@ -10,17 +10,39 @@ public class sample14 {
 	public static void main(String[] args) {
 		ArrayList<String> team = new ArrayList<String>();
 
+		// 要素の追加
 		team.add("１番");
 		team.add("２番");
 		team.add("３番");
 		for (String namber : team) {
 			System.out.println(namber);
 		}
+		System.out.println("");
 
 		// 要素の取り出し
+		System.out.println("３番目の要素をゲット");
 		System.out.println(team.get(2));
-		// 配列の長さの取得
-		System.out.println(team.size());
+		System.out.println("");
+		// 要素数の取得
+		System.out.println("要素数は" + team.size());
+		System.out.println("");
+
+		// 要素の変更
+		System.out.println("変更後のArrayList");
+		team.set(2,"５番");
+		for (String namber : team) {
+			System.out.println(namber);
+		}
+		System.out.println("");
+
+		// 要素の削除
+		System.out.println("削除後のArrayList");
+		team.remove(1);
+		for (String namber : team) {
+			System.out.println(namber);
+		}
+		// 要素数の取得
+			System.out.println("要素数は" + team.size());
 
 	}
 
